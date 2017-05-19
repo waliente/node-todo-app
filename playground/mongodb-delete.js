f18341bef3ddb30cef9ee053e7d3d53799b795e6
mchierchia <mchierchia@engagepeople.com>
2017-05-19 15:13:06 +0200

    Delete function on MongoDB

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,36 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// connetti al database, primo argomento url al DB, secondo argomento una callback
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+
+  if (err) {
+    return console.log('Unable to connect to MongoDB server')
+  }
+
+  console.log('Connected to MongoDB server');
+
+  // deleteMany
+  db.collection('Todos')
+      .deleteMany({text: 'eat lunch'})
+      .then((result) => {
+        console.log(result);
+      });
+
+  // deleteOne
+  db.collection('Todos')
+      .deleteOne({text: 'eat lunch'})
+      .then((result) => {
+        console.log(result);
+      });
+
+  // findOneAndDelete
+  db.collection('Todos')
+      .findOneAndDelete({completed: false})
+      .then((result) => {
+        console.log(result);
+      });
+
+  // db.close();
+
+});
